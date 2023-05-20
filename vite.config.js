84c77667fbbfd160b3ce0062956539c8ea0c2912
John Mirage <28307090+john-mirage@users.noreply.github.com>
2023-05-20 13:54:06 +0200

    fix: base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/testimonials-grid-section/",
   resolve: {
     alias: {
       '@assets': resolve(__dirname, 'src/assets'),
